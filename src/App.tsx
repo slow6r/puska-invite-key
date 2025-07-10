9add989deac899a2f0de51586868fcf1b23b4ea1
Фёдор Лодиков <fedor_ramoz@mail.ru>
2025-07-10 14:40:19 +0300

    fix: 1.123.5

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ function MusicController({
 
   React.useEffect(() => {
     if (audioRef.current) {
-      audioRef.current.volume = 0.5;
+      audioRef.current.volume = 0.1;
       audioRef.current.muted = muted;
       if (playing) {
         audioRef.current.play().catch(() => {});
